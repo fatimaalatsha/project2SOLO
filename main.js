633ea060ab6893f0e0d19f6dc68677ff8e009ca8
fatima alatsha <70060124+fatimaalatsha@users.noreply.github.com>
2020-10-07 23:48:54 +0300

    p4

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
+
+////sing in code
 var data={
 	username:'fatima',
 	password:1234
 }
-
-// 	$('#return-to-main').click(function(){
-// 		$(".colosseum").show()
-// 		$(".colosseum3").hide()
-// 		$(".Taj-Mahal").show()
-// 		$(".Taj-Mahal3").hide()
-
-// })
-
-
-
-
+ //willcj=heck if the username and passwork are correct
 $('#submit').click(function(){
 	var x=$("#username").val();
  var y=$("#password").val();
@@ -31,25 +22,91 @@ alert("Please fill all fields.");
 else
 	alert("somting is wrong, Please check your username and password")
 	
-	// $("#username").val('');
- //    $("#password").val('');	
+})
+$('#withoutsigning').click(function(){
+	$(".next-page").show();
+	$(".signing").hide();
+
 })
  
-// $('#move').click(function(){
-// var time=$('#time :selected').text();;
-// console.log(time)
-// var three= $('#3')
-// if (time === three ){
-// 	$(".next-page").hide();
-//     $("#three").show();
-// }
-
-
-
-// })
- $(".three").append(('<img id="taj7" src="https://whc.unesco.org/uploads/thumbs/site_0252_0032-360-360-20131015115606.jpg"/>'))
+ //array of objects
+ // var all=[history={
+ // 	three:$(".threeMin").append("This hugely successful film won a multitude of international awards.[2] The film received a Genie Award nomination for Best Theatrical Short Film at the 2nd Genie Awards in 1981,[3] and an Academy Award nomination for Best Animated Short Film [4] at the 53rd Academy Awards.[5] It won the award for Best Film Under Three Minutes at the 1980 Ottawa International Animation Festival,[6] the Short Film Golden Bear at the 1981 Berlin Film Festival,[1] a Golden Reel Award from the Canada Council's inaugural Canadian Short Films Showcase,[7] and the American Film and Video Festival's Blue Ribbon Award.[2]"),
+ // 	five:1,
+ // 	ten:1
+ // },
+ // philosophy={
+ // 	three:1,
+ // 	five:1,
+ // 	ten:1
+ // },
+ // Sciences={
+ // 	three:1,
+ // 	five:1,
+ // 	ten:1
+ // }]
+//the buttons to move beteen the pages
+//three minuts functions
 $("#btn3").click(function(){
 	$(".next-page").hide();
     $(".three").show();
-    $(".signing").hide();
+
+})
+//to choose the catogory
+//history 3 min
+$("#History3").click(function(){
+    $(".three").hide();
+    $('.threeMinHistory').show();
+    
+})
+$('#return').click(function(){
+ $(".three").show();
+    $('.threeMinHistory').hide();
+})
+//three Minp hilosophy
+$("#philosophy3").click(function(){
+    $(".three").hide();
+    $('.threeMinphilosophy').show();
+    
+})
+$('#return1').click(function(){
+ $(".three").show();
+    $('.threeMinphilosophy').hide();
+})
+//five min history
+
+$("#btn5").click(function(){
+	$(".next-page").hide();
+    $(".five").show();
+    
+})
+$("#Art5").click(function(){
+    $(".five").hide();
+    $('.fiveMinArt').show();
+    
+})
+$('#return2').click(function(){
+$(".five").show();
+    $('.fiveMinArt').hide();
+})
+
+$("#astronomy5").click(function(){
+    $(".five").hide();
+    $('.fiveMinastronomy').show();
+    
+})
+$('#return3').click(function(){
+$(".five").show();
+    $('.fiveMinastronomy').hide();
+})
+
+$('#header').click(function(){
+ $(".next-page").show();
+ $(".three").hide();
+ $('.threeMinHistory').hide();
+ $('.threeMinphilosophy').hide();
+ $(".five").hide();
+ $('.fiveMinArt').hide();
+ $('.fiveMinastronomy').hide();
+
 })
