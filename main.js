1edb545b36fdb69978723b7182175deaa0147bff
fatima alatsha <70060124+fatimaalatsha@users.noreply.github.com>
2020-10-07 03:46:24 +0300

    projectP1

---
+++ main.js
@@ -0,0 +1,23 @@
+// var data={
+// 	username:"fatima",
+// 	password:"1234"
+// }
+var x=$("#username").val();
+var y=$("#password").val();
+// 	$('#return-to-main').click(function(){
+// 		$(".colosseum").show()
+// 		$(".colosseum3").hide()
+// 		$(".Taj-Mahal").show()
+// 		$(".Taj-Mahal3").hide()
+
+// })
+$('.submit').click(function(){
+	if (x === fatima) && (y === 1234){
+		$(".next-page").show();
+		$(".signing").hide();
+	}
+	else 
+	alert("try again")	
+
+		
+})
