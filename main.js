05504458756affa66604063514d00ad4a50f5ef3
fatima alatsha <70060124+fatimaalatsha@users.noreply.github.com>
2020-10-07 18:04:13 +0300

    p3

--- main.js
+++ main.js
@@ -11,12 +11,17 @@ var data={
 
 // })
 
+
+
+
 $('#submit').click(function(){
 	var x=$("#username").val();
-    var y=$("#password")
-	if (data.username === x && data.password === y ){
+ var y=$("#password").val();
+	if (data.username === x || data.password === y ){
+		$("#hello").append(x)
 		$(".next-page").show();
 		$(".signing").hide();
+
 	}
 	 
 	else if ( x ==='' || y ===''){
@@ -28,4 +33,23 @@ else
 	
 	// $("#username").val('');
  //    $("#password").val('');	
+})
+ 
+// $('#move').click(function(){
+// var time=$('#time :selected').text();;
+// console.log(time)
+// var three= $('#3')
+// if (time === three ){
+// 	$(".next-page").hide();
+//     $("#three").show();
+// }
+
+
+
+// })
+ $(".three").append(('<img id="taj7" src="https://whc.unesco.org/uploads/thumbs/site_0252_0032-360-360-20131015115606.jpg"/>'))
+$("#btn3").click(function(){
+	$(".next-page").hide();
+    $(".three").show();
+    $(".signing").hide();
 })
