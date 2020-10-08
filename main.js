f08bcab38cc122a91ba0fab70d355dd087a37448
fatima alatsha <70060124+fatimaalatsha@users.noreply.github.com>
2020-10-08 03:59:24 +0300

    e

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var data={
 	password:1234
 }
  //willcj=heck if the username and passwork are correct
-$('#submit').click(function(){
+$('#submit').click(function  (){
 	var x=$("#username").val();
  var y=$("#password").val();
 	if (data.username === x || data.password === y ){
@@ -23,28 +23,16 @@ else
 	alert("somting is wrong, Please check your username and password")
 	
 })
-$('#withoutsigning').click(function(){
+
+
+
+$('#withoutsigning').click(function (){
 	$(".next-page").show();
 	$(".signing").hide();
 
 })
  
- //array of objects
- // var all=[history={
- // 	three:$(".threeMin").append("This hugely successful film won a multitude of international awards.[2] The film received a Genie Award nomination for Best Theatrical Short Film at the 2nd Genie Awards in 1981,[3] and an Academy Award nomination for Best Animated Short Film [4] at the 53rd Academy Awards.[5] It won the award for Best Film Under Three Minutes at the 1980 Ottawa International Animation Festival,[6] the Short Film Golden Bear at the 1981 Berlin Film Festival,[1] a Golden Reel Award from the Canada Council's inaugural Canadian Short Films Showcase,[7] and the American Film and Video Festival's Blue Ribbon Award.[2]"),
- // 	five:1,
- // 	ten:1
- // },
- // philosophy={
- // 	three:1,
- // 	five:1,
- // 	ten:1
- // },
- // Sciences={
- // 	three:1,
- // 	five:1,
- // 	ten:1
- // }]
+ 
 //the buttons to move beteen the pages
 //three minuts functions
 $("#btn3").click(function(){
@@ -52,12 +40,17 @@ $("#btn3").click(function(){
     $(".three").show();
 
 })
+
+$("#quiz").click(function(){
+	$(".next-page").hide();
+	$('.pop-quiz').show();
+})
 //to choose the catogory
 //history 3 min
 $("#History3").click(function(){
     $(".three").hide();
     $('.threeMinHistory').show();
-    
+    $(".quiz-container").show();
 })
 $('#return').click(function(){
  $(".three").show();
@@ -100,13 +93,17 @@ $(".five").show();
     $('.fiveMinastronomy').hide();
 })
 
-$('#header').click(function(){
- $(".next-page").show();
- $(".three").hide();
- $('.threeMinHistory').hide();
- $('.threeMinphilosophy').hide();
- $(".five").hide();
- $('.fiveMinArt').hide();
- $('.fiveMinastronomy').hide();
+// $('#header').click(function(){
+//  $(".next-page").show();
+//  $(".three").hide();
+//  $('.threeMinHistory').hide();
+//  $('.threeMinphilosophy').hide();
+//  $(".five").hide();
+//  $('.fiveMinArt').hide();
+//  $('.fiveMinastronomy').hide();
 
-})
+//the quizs
+
+
+
+
